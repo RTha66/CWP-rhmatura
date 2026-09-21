f348dcf0a23b8fd6587322267a7799bdda8d91ec
RTha <66070175@kmitl.ac.th>
2026-09-21 15:48:32 +0700

    Add new scripts for password validation, multiplication, and number input handling

---
+++ discovery_piscine/cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "Python is awesome"
+user_input = input().strip()
+if user_input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
---
+++ discovery_piscine/cell03/ex03/multiplication_table.py
@@ -0,0 +1,5 @@
+print("Enter a number")
+number = int(input())
+for i in range(10):
+    result = i * number
+    print(f"{i} x {number} = {result}")
