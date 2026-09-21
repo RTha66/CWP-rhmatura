9d835de2e492fb0cd454d223f34d4b0f6d070071
RTha <66070175@kmitl.ac.th>
2026-09-21 15:23:53 +0700

    Add age calculation, name greeting, zero check, and negativity check scripts

---
+++ discovery_piscine/cell01/ex02/age.py
@@ -0,0 +1,4 @@
+current_age = 1
+added_years = 42 
+my_age = current_age + added_years 
+print(my_age)
---
+++ discovery_piscine/cell02/ex00/iszero.py
@@ -0,0 +1,5 @@
+number = input()
+if int(number) == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
